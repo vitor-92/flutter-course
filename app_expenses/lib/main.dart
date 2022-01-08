78e575e97703075bceda53b3f4c51b7645bebf9b
vitor santos <vitor.documents@gmail.com>
2022-01-08 10:33:53 -0300

    add app expenses base

---
+++ app_expenses/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: const Center(
+        child: Text('Widget Playground!'),
+      ),
+    );
+  }
+}
